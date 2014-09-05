b2dd42449ac5eac0eaa07aa843e031026157be11
kathybarahona03 <kathia.barahona@unitec.edu>
2014-09-05 15:27:08 -0600

    Clase Forense incluida

---
+++ forense.cpp
@@ -0,0 +1,24 @@
+#include "persona.h"
+#include "forense.h"
+#include<sstream>
+#include<string>
+Forense::Forense(string nombre,string usuario,string password,int edad
+,string id,string fecha,string ingreso,string horario):Persona(nombre,usuario,password,edad,id,fecha),ingreso(ingreso),horario(horario){}
+string Forense::toString()const{
+	stringstream ss;
+	ss<<Persona::toString()<<"\nIngreso: "<<ingreso<<"\nHorario: "<<horario;
+	return ss.str();
+}
+string Forense::getIngreso()const{
+	return ingreso;
+}
+string Forense::getHorario()const{
+	return horario;
+}
+void Forense::setIngreso(string ingreso){
+	this->ingreso=ingreso;
+}
+void Forense::setHorario(string horario){
+	this->horario=horario;
+}
+//void Forense:: visualizar(Caso c){}

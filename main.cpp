ccdd0052556f1a8ddceeec03684af7b10a9576b3
Xavier Munguia <xavier2696@gmail.com>
2014-09-12 15:11:36 -0600

    Main: agregar casos

--- main.cpp
+++ main.cpp
@@ -167,9 +167,111 @@ int main(int argc,char* argv[]){
 				cout<<"2-Secuestro:"<<endl;
 				cin>>opcion3;
 				if(opcion3==1){
-
+					int numero;
+					vector<Evidencia*> evidencias;
+					string fecha, hora;
+					bool cerrado;
+					vector<string> nombres;
+					string sospechoso, victima;
+					cout << "Ingrese el numero del caso: ";
+					cin >> numero;
+					cout << "Evidencias "<<endl;
+					for (int i = 0; i<ls_evidencias.size(); i++){
+						cout <<"["<< ls_evidencias[i]->getnombre()<<"]";
+					}
+					int posicion;
+					do{
+						cout << "Ingrese la posicion de la evidencia que desea agregar(Ingrese -1 para dejar de agregar):";
+						cin >> posicion;
+						if(posicion>=0 && posicion<ls_evidencias.size()){
+							Evidencia* e = new Evidencia(evidencias[posicion]->getnombre(),evidencias[posicion]->gettipo_objeto(),evidencias[posicion]->getlugar_encontrada(),evidencias[posicion]->gethuella(),evidencias[posicion]->getprocesada());
+							evidencias.push_back(e);						
+						}
+					}while(posicion>=0);
+					cout << "Ingrese la fecha: ";
+					cin >> fecha;
+					cout << "Ingrese la hora: ";
+					cin >> hora;
+					char respuesta;
+					cout << "Ingrese s si esta cerrado el caso: ";
+					cin >> respuesta;
+					if(respuesta == 's')
+						cerrado = true;
+					else
+						cerrado = false;
+					string nombretemp;
+					do{
+						cout << "Ingrese los nombres sospechosos(0 para dejar de ingresar): ";
+						cin >> nombretemp;
+						if (nombretemp != "0")
+							nombres.push_back(nombretemp);
+					}while(nombretemp != "0");
+					cout <<"Ingrese el sospechoso principal: ";
+					cin >> sospechoso;
+					cout << "Ingrese la victima: ";
+					cin >> victima;
+					Homicidio* temp = new Homicidio(numero, evidencias,fecha, hora, cerrado, nombres, sospechoso, victima); 
+					ls_caso.push_back(temp);
 				}else if(opcion3==2){
-
+					int numero;
+					vector<Evidencia*> evidencias;
+					string fecha, hora;
+					bool cerrado;
+					string victima, motivo, lugar;
+					bool rescate;
+					int cantidad;
+					bool vivo;
+					cout << "Ingrese el numero del caso: ";
+					cin >> numero;
+					cout << "Evidencias "<<endl;
+					for (int i = 0; i<ls_evidencias.size(); i++){
+						cout <<"["<< ls_evidencias[i]->getnombre()<<"]";
+					}
+					int posicion;
+					do{
+						cout << "Ingrese la posicion de la evidencia que desea agregar(Ingrese -1 para dejar de agregar):";
+						cin >> posicion;
+						if(posicion>=0 && posicion<ls_evidencias.size()){
+							Evidencia* e = new Evidencia(evidencias[posicion]->getnombre(),evidencias[posicion]->gettipo_objeto(),evidencias[posicion]->getlugar_encontrada(),evidencias[posicion]->gethuella(),evidencias[posicion]->getprocesada());
+							evidencias.push_back(e);						
+						}
+					}while(posicion>=0);
+					cout << "Ingrese la fecha: ";
+					cin >> fecha;
+					cout << "Ingrese la hora: ";
+					cin >> hora;
+					char respuesta;
+					cout << "Ingrese s si esta cerrado el caso: ";
+					cin >> respuesta;
+					if(respuesta == 's')
+						cerrado = true;
+					else
+						cerrado = false;
+					cout << "Ingrese la victima: ";
+					cin >> victima;
+					cout << "Ingrese el motivo: ";
+					cin >> motivo;
+					cout << "Ingrese el lugar: ";
+					cin >> lugar;
+					cout << "Ingrese s si pide rescate: ";
+					cin >> respuesta;
+					if(respuesta = 's')
+						rescate = true;
+					else
+						rescate = false;
+					if(rescate){
+						cout << "Ingrese la cantidad de dinero que solicitan: ";
+						cin >> rescate;
+					}else
+						rescate = 0;
+					cout <<"Ingrese s si esta vivo: ";
+					cin >> respuesta;
+					if(respuesta ='s')
+						vivo = true;
+					else
+						vivo = false;
+					Secuestro* temp = new Secuestro(numero,evidencias,fecha,hora, cerrado, victima, motivo, lugar, rescate, cantidad, vivo);
+					ls_caso.push_back(temp);
 				}else {
 					cout<<"OPCION NO ENCONTRADA"<<endl;
 				}
